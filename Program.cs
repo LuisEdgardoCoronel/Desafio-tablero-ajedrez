bac20c54c50efe40c442dfd2398904326a9ffdd9
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2023-12-26 22:16:58 -0300

    Update Program.cs
    creo el objeto tablero y llamo a sus metodos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Desafio_tablero_ajedrez;
+
+Tablero tablero = new(8);
+tablero.RellenarTablero();
+tablero.MostrarTablero();
+
+
